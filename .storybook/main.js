bbf40ec0cb90acd719e9d593606d38cce7099106
Alistair Smith <alistairsmith01@gmail.com>
2021-09-01 22:50:55 +0100

    remove sb

--- .storybook/main.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	stories: ['../stories/**/*.stories.@(ts|tsx|js|jsx)'],
-	addons: ['@storybook/addon-links', '@storybook/addon-essentials', 'storybook-dark-mode'],
-	typescript: {
-		check: true,
-	},
-};
