bac2f01997d0375b280b7604caaff9c39872b412
Alistair Smith <alistairsmith01@gmail.com>
2021-10-24 03:08:10 +0100

    bedtime

--- packages/emoji/src/picker.tsx
+++ packages/emoji/src/picker.tsx
@@ -6,6 +6,7 @@ import {chunk} from './util';
 import {Cell} from './cell';
 import {Category} from './category';
 import {Container} from './container';
+import {FixedSizeList} from 'react-window';
 
 export interface EmojiProps extends PropsFor<'div'> {
 	/**
